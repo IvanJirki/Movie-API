3756cd77fcf8b2cb55b74820632757b47964f991
IvanJirki <c3jiiv00@students.oamk.fi>
2024-11-24 02:47:31 +0200

    json lisää koodi

--- server.js
+++ server.js
@@ -2,24 +2,20 @@ import express from 'express';
 import multer from 'multer';
 
 const app = express();
-const unload = multer ({dest: 'upload/'})
+const upload = multer({ dest: 'upload/' }); // Korjattu muuttujan nimi
 
-app.use(express.urlencoded({extended: true}));
-app.use(upload.none() );
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.none());
 app.use(express.json());
 
-app.listen(3001,()=>{
-    console.log('Server runing..');
-
-})
-
-app.get('/user', (req, res) =>{
-    let user = req.destroyed;
-    console.log (user);
-
-    res.send();
+app.listen(3001, () => {
+    console.log('Server running on http://localhost:3001');
 });
 
-app.post('/user', (req, res) =>{
+app.get('/user', (req, res) => {
+    // Korjataan virheellinen muuttujaviittaus
+    let user = req.query.user; // Oletetaan, että käyttäjädata tulee kyselyparametrina
+    console.log(user);
 
+    res.send({ message: 'User data logged!' });
 });
