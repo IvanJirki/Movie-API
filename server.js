8a99adc3d5c3371514beb2f15230edf7f6695a91
IvanJirki <c3jiiv00@students.oamk.fi>
2024-11-24 02:16:29 +0200

    server.json lisää koodi

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import multer from 'multer';
 
 const app = express();
+const unload = multer ({dest: 'upload/'})
+
+app.use(express.urlencoded({extended: true}));
+app.use(upload.none() );
+app.use(express.json());
 
 app.listen(3001,()=>{
     console.log('Server runing..');
 
 })
+
+app.get('/user', (req, res) =>{
+    let user = req.destroyed;
+    console.log (user);
+
+    res.send();
+});
+
+app.post('/user', (req, res) =>{
+
+});
